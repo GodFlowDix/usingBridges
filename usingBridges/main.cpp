f1a88a7d35b823bf8ea15299ad024e14da8be547
Money Mike <mikedix25@yahoo.com>
2019-01-14 16:42:08 -0500

    Initial Commit

---
+++ usingBridges/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  usingBridges
+//
+//  Created by Mike Dix on 1/14/19.
+//  Copyright © 2019 Mike Dix. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
